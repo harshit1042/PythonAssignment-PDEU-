7c9a7554510ac789ec5703805cdf8cce9c7517c2
harshit1042 <105489003+harshit1042@users.noreply.github.com>
2022-05-23 19:02:59 +0530

    Add files via upload

---
+++ Assignment6.py
@@ -0,0 +1,7 @@
+d = {}
+for i in range(1, 11):
+    adm_no = int(input("Enter admission number of student:"))
+    name = input("Enter name of student:")
+    d[adm_no] = name
+
+print("dictionary is :", d)
